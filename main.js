a96dc37cdea9f398b333db4bb82a760ebab8ef44
nur <medo0o6665@gmail.com>
2022-04-21 16:38:14 +0800

    improving adding block to blockchain and giving Reward to miner and creating Transaction class

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const SHA256 = require("crypto-js/sha256");
-
+class Transaction {
+  constructor(fromAdders, toAdders, amount) {
+    this.fromAdders = fromAdders;
+    this.toAdders = toAdders;
+    this.amount = amount;
+  }
+}
+// 1 + 3 + 1 = 5
+// timestamp + transaction + perviousHash + hash + nonce = "000kkjadafaaec"
 class Block {
-  constructor(index, timestamp, data, perviousHash) {
-    this.index = index;
+  constructor(timestamp, transaction, perviousHash) {
     this.timestamp = timestamp;
-    this.data = data;
+    this.transaction = transaction;
     this.perviousHash = perviousHash;
     this.hash = this.calculateHash();
     this.nonce = 0;
@@ -12,10 +19,9 @@ class Block {
 
   calculateHash() {
     return SHA256(
-      this.index +
-        this.perviousHash +
+      this.perviousHash +
         this.timestamp +
-        JSON.stringify(this.data) +
+        JSON.stringify(this.transaction) +
         this.nonce
     ).toString();
   }
@@ -36,23 +42,63 @@ class Block {
 class BlockChain {
   constructor() {
     this.chain = [this.createGenesisBlock()];
-    this.difficulty = 4;
+    this.difficulty = 2;
+    this.pendingTransactions = [];
+    this.miningReward = 100;
   }
 
   createGenesisBlock() {
-    return new Block(0, "1/1/2017", "Genesis Block", "0");
+    return new Block("1/1/2017", "Genesis Block", "0");
   }
 
   getLastBlock() {
     return this.chain[this.chain.length - 1];
   }
 
-  addBlock(newBlock) {
-    newBlock.perviousHash = this.getLastBlock().hash;
-    newBlock.minBlock(this.difficulty);
-    this.chain.push(newBlock);
+  //   addBlock(newBlock) {
+  //     newBlock.perviousHash = this.getLastBlock().hash;
+  //     newBlock.minBlock(this.difficulty);
+  //     this.chain.push(newBlock);
+  //   }
+
+  minPendingTransactions(miningRewardAddress) {
+    let block = new Block(Date.now(), this.pendingTransactions);
+    block.minBlock(this.difficulty);
+
+    console.log("Block successfully mined");
+
+    this.chain.push(block);
+
+    this.pendingTransactions = [
+      new Transaction(
+        null,
+        miningRewardAddress,
+        this.miningReward
+      ),
+    ];
   }
 
+  createTransaction(transaction) {
+    this.pendingTransactions.push(transaction);
+  }
+
+  getBalanceOfAddress(address) {
+    let balance = 0;
+
+    for (const block of this.chain) {
+      for (const transaction of block.transaction) {
+        if (transaction.fromAdders === address) {
+          balance -= transaction.amount;
+        }
+
+        if (transaction.toAdders === address) {
+          balance += transaction.amount;
+        }
+      }
+    }
+
+    return balance;
+  }
   isChainValid() {
     for (let i = 1; i < this.chain.length; i++) {
       const currentBlock = this.chain[i];
@@ -70,12 +116,30 @@ class BlockChain {
 }
 
 let savjeCoin = new BlockChain();
-console.log("mining block 1...");
+savjeCoin.createTransaction(
+  new Transaction("address1", "address2", 100)
+);
+savjeCoin.createTransaction(
+  new Transaction("address2", "address1", 50)
+);
+console.log("starting mining...");
+savjeCoin.minPendingTransactions("nur");
 
-savjeCoin.addBlock(new Block(2, "2/2/2017", { amount: 4 }));
+console.log(savjeCoin.getBalanceOfAddress("nur"));
 
-console.log("mining block 2...");
-savjeCoin.addBlock(new Block(3, "3/2/2017", { amount: 41 }));
+console.log("starting mining again...");
+savjeCoin.minPendingTransactions("nur");
+
+console.log(savjeCoin.getBalanceOfAddress("nur"));
+console.log(savjeCoin.getBalanceOfAddress("address1"));
+console.log(savjeCoin.getBalanceOfAddress("address2"));
+
+// console.log("mining block 1...");
+
+// savjeCoin.addBlock(new Block(2, "2/2/2017", { amount: 4 }));
+
+// console.log("mining block 2...");
+// savjeCoin.addBlock(new Block(3, "3/2/2017", { amount: 41 }));
 
 // console.log(JSON.stringify(savjeCoin, null, 4));
 // console.log(`is blockchain valid: ${savjeCoin.isChainValid()}`);
